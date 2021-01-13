0139fea9e6b64e9c18c4b0024c7c814d3cb2845c
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-13 23:01:40 +0000

    State Interface - Coded the State Interface Script to be Implemented

--- src/Main/stateMachine/State.gd
+++ src/Main/stateMachine/State.gd
@@ -1,2 +1,31 @@
 extends Node
 class_name State, "res://assets/icons/state.svg"
+
+"""
+State Interface to use in Hierarchical State Machines.
+"""
+
+onready var _state_machine := _get_state_machine(self)
+
+
+func unhandled_input(event: InputEvent) -> void:
+	return
+
+
+func physics_process(delta: float) -> void:
+	return
+
+
+func enter(msg: Dictionary = {}) -> void:
+	return
+
+
+func exit() -> void:
+	return
+
+
+func _get_state_machine(node: Node) -> Node :
+	if node != null and not node.is_in_group("state_machine"):
+		return _get_state_machine(node.get_parent())
+	
+	return node
