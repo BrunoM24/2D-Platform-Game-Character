5baa8176c0281c430105fd077dca3a074a5eb838
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-16 16:01:40 +0000

    Player - States - Moved the Jump Logic from the Move State to the Air State to allow Double Jumps

--- src/Player/states/Air.gd
+++ src/Player/states/Air.gd
@@ -2,10 +2,19 @@ extends State
 
 
 export var acceleration_x = 5000.0
+export var jump_impulse := 900.0
+export var max_jump_count := 2
+
+var _jump_counting := 0
 
 
 func unhandled_input(event: InputEvent) -> void:
-	get_parent().unhandled_input(event)
+	var move := get_parent()
+	
+	if event.is_action_pressed("jump") and _jump_counting < max_jump_count:
+		jump()
+	
+	move.unhandled_input(event)
 
 
 func physics_process(delta: float) -> void:
@@ -15,7 +24,6 @@ func physics_process(delta: float) -> void:
 	
 	if owner.is_on_floor():
 		var target_state := "Move/Idle" if move.get_move_direction().x == 0.0 else "Move/Run"
-		
 		_state_machine.transition_to(target_state)
 
 
@@ -29,13 +37,23 @@ func enter(msg: Dictionary = {}) -> void:
 		move.max_speed.x = max(abs(msg.velocity.x), move.max_speed.x)
 	
 	if "impulse" in msg:
-		move.velocity += calculate_jump_velocity(msg.impulse)
+		jump()
+	else:
+		_jump_counting += 1
 
 
 func exit() -> void:
 	var move := get_parent()
-	move.exit()
 	move.acceleration = move.acceleration_default
+	_jump_counting = 0
+	move.exit()
+
+
+func jump() -> void:
+	var move := get_parent()
+	move.velocity.y = 0
+	move.velocity += calculate_jump_velocity(jump_impulse)
+	_jump_counting += 1
 
 
 func calculate_jump_velocity(impulse := 0.0) -> Vector2:
--- src/Player/states/Move.gd
+++ src/Player/states/Move.gd
@@ -5,10 +5,10 @@ Parent state that abstracts and handles basic movement
 Move related children states can delegate movement to it, or use its utility functions
 """
 
-export var max_speed_default := Vector2(500.0, 1500.0)
+export var max_speed_default := Vector2(500.0, 900.0)
 export var acceleration_default := Vector2(100000.0, 3000.0)
 export var decceleration_default := Vector2(500.0, 3000.0)
-export var jump_impulse := 900.0
+export var max_speed_fall := 1500.0
 
 var max_speed := max_speed_default
 var acceleration := acceleration_default
@@ -18,18 +18,22 @@ var velocity := Vector2.ZERO
 
 func unhandled_input(event: InputEvent) -> void:
 	if owner.is_on_floor() and event.is_action_pressed("jump"):
-		_state_machine.transition_to("Move/Air", {impulse = jump_impulse})
+		_state_machine.transition_to("Move/Air", {impulse = true})
 
 
 func physics_process(delta: float) -> void:
-	velocity = calculate_velocity(velocity, max_speed, acceleration, decceleration, delta, get_move_direction())
+	velocity = calculate_velocity(velocity, max_speed, acceleration, decceleration, delta, get_move_direction(), max_speed_fall)
 	
 	velocity = owner.move_and_slide(velocity, owner.FLOOR_NORMAL)
 	
 	Events.emit_signal("player_moved", velocity)
 
 
-static func calculate_velocity(old_velocity: Vector2, max_speed: Vector2, acceleration: Vector2, decceleration: Vector2, delta: float, move_direction: Vector2) -> Vector2:
+static func calculate_velocity(old_velocity: Vector2, max_speed: Vector2, 
+		acceleration: Vector2, decceleration: Vector2, delta: float, 
+		move_direction: Vector2, max_speed_fall := 1500.0
+	) -> Vector2:
+	
 	var new_velocity := old_velocity
 	
 	new_velocity.y += move_direction.y * acceleration.y * delta
@@ -41,7 +45,7 @@ static func calculate_velocity(old_velocity: Vector2, max_speed: Vector2, accele
 		new_velocity.x = new_velocity.x if sign(new_velocity.x) == sign(old_velocity.x) else 0
 	
 	new_velocity.x = clamp(new_velocity.x, -max_speed.x, max_speed.x)
-	new_velocity.y = clamp(new_velocity.y, -max_speed.y, max_speed.y)
+	new_velocity.y = clamp(new_velocity.y, -max_speed.y, max_speed_fall)
 	
 	return new_velocity
 
